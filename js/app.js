7620a5080788e7857613e2b9983bb0588838cc80
Raghav Pahwa <raghav8822@gmail.com>
2019-07-02 23:12:23 +0530

    Changed animation from JQUERY TO DOM manipulation through javascript only

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-$(document).ready(
-    function () {
-        $('.heading').addClass("animated zoomIn");
-        $('.navbar-brand').addClass("animated slideInRight");
-    });
+// $(document).ready(
+//     function () {
+//         $('.heading').addClass("animated zoomIn");
+//         $('.navbar-brand').addClass("animated slideInRight");
+//     });
+
+let heading = document.querySelectorAll('.heading')
+heading.forEach(function (headingElement) {
+    headingElement.className += ' animated zoomIn'
+});
+//heading.className += ' animated zoomIn';
+let navbarBrand = document.querySelector('.navbar-brand')
+navbarBrand.className += ' animated slideInRight';
